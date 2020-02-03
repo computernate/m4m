6df64e815bea43e5f9ec52f87c93c1ccd83a5cde
computernate <nateroskelley@gmail.com>
2020-02-03 15:32:25 -0700

    Fixed createDatabase bug

--- PHP/createDatabase.php
+++ PHP/createDatabase.php
@@ -1,7 +1,7 @@
 CREATE DATABASE money_4_memes;
 
-CREATE TABLE users(id varchar(31) PRIMARY KEY, name varchar(255), password varchar (255), email varchar (255), popularMemes int (31), earnings double (15), admin int(2), bankingID varchar (255), cart varchar (2047));
-INSERT INTO users VALUES ("0.nqjdr90iqi", 'Nathan', "9315", "nateroskelley@gmail.com",0,0,1);
+CREATE TABLE users(id varchar(31) PRIMARY KEY, name varchar(255), password varchar (255), email varchar (255), popularMemes int (31), earnings double (15,2), admin int(2), bankingID varchar (255), cart varchar (2047));
+INSERT INTO users VALUES ("0.nqjdr90iqi", 'Nathan', "9315", "nateroskelley@gmail.com",0,0,1,"","");
 
 CREATE TABLE memes( id VARCHAR(31) PRIMARY KEY, title varchar(511), pointerID varchar (31), likes int(15), age int(255), score int (127),
 	hasShirt tinyint, tags varchar (511), description varchar (255), fileType varchar (8), isPrivate tinyint);
